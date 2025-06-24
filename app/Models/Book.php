9ae48f85b19f51073fbbd0a75176e1a241465bc1
Kaique Parente da Silva <kaique.parente.game@outlook.com>
2025-06-24 14:57:57 -0300

    [ADD] Factory e Local scope

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -2,11 +2,20 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
 
 class Book extends Model
 {
+    /** @use HasFactory<\Database\Factories\TaskFactory> */
+    use HasFactory, Notifiable;
     public function reviews(){
         return $this->hasMany(Review::class);
+    }
+
+    public function scopeTitle(Builder $query, $title): Builder{
+        return $query->where("title","LIKE","%". $title ."%");
     }
 }
--- app/Models/Review.php
+++ app/Models/Review.php
@@ -2,10 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
 
 class Review extends Model
 {
+    /** @use HasFactory<\Database\Factories\TaskFactory> */
+    use HasFactory, Notifiable;
     public function book(){
         return $this->belongsTo(Book::class);
     }

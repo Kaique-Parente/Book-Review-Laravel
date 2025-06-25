d836a14281fb4d8dd6673df2efdfc181594b5e8c
Kaique Parente da Silva <kaique.parente.game@outlook.com>
2025-06-24 21:28:49 -0300

    [ADD] Scopes com data

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -11,21 +11,41 @@ class Book extends Model
 {
     /** @use HasFactory<\Database\Factories\TaskFactory> */
     use HasFactory, Notifiable;
-    public function reviews(){
+    public function reviews()
+    {
         return $this->hasMany(Review::class);
     }
 
-    public function scopeTitle(Builder $query, $title): Builder{
-        return $query->where("title","LIKE","%". $title ."%");
+    public function scopeTitle(Builder $query, $title): Builder
+    {
+        return $query->where("title", "LIKE", "%" . $title . "%");
     }
 
-    public function scopePopular(Builder $query): Builder{
-        return $query->withCount("reviews")
-            ->orderBy("reviews_count","desc");
+    public function scopePopular(Builder $query, $from = null, $to = null): Builder
+    {
+        return $query->withCount([
+            'reviews' => fn(Builder $q) => $this->dateRangeFilter($q, $from, $to)
+        ])
+            ->orderBy("reviews_count", "desc");
     }
 
-    public function scopeHighestRated(Builder $query): Builder{
-        return $query->withAvg("reviews", "rating")
-            ->orderBy("reviews_avg_rating","desc");
+    public function scopeHighestRated(Builder $query, $from = null, $to = null): Builder
+    {
+        return $query->withAvg([
+            "reviews"=> fn(Builder $q) =>  $this->dateRangeFilter($q, $from, $to)
+        ], 'rating')
+            ->orderBy("reviews_avg_rating", "desc");
+    }
+    private function dateRangeFilter(Builder $query, $from = null, $to = null): Builder
+    {
+        if ($from && !$to) {
+            return $query->where('created_at', '>=', $from);
+        } elseif (!$from && $to) {
+            return $query->where('created_at', '<=', $to);
+        } elseif ($from && $to) {
+            return $query->whereBetween('created_at', [$from, $to]);
+        }
+
+        return $query;
     }
 }

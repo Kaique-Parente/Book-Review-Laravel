8224eb77c9735e4373e17459b66eca5aecce8439
Kaique Parente da Silva <kaique.parente.game@outlook.com>
2025-06-25 14:43:59 -0300

    [REFACTOR] Método de mostrar livro exibir rating e quantidade de reviews utilizando cache

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -54,14 +54,18 @@ class BookController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(Book $book)
+    public function show(int $id)
     {
-        $cachekey = "book:". $book->id;
+        $cachekey = "book:". $id;
 
-        $book = cache()->remember($cachekey,3600,
-            fn() => $book->load([
+        $book = cache()->remember(
+            $cachekey,
+            3600,
+            fn() =>
+            Book::with([
                 'reviews' => fn($query) => $query->latest()
-        ]));
+            ])->withAvgRating()->withReviewsCount()->findOrFail($id)
+        );
 
         return view('books.show', ['book' => $book]);
     }
